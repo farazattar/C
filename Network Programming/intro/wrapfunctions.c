7cd6afc03e33d272d7902089c559c3561382870d
farazattar <63309056+farazattar@users.noreply.github.com>
2023-10-02 14:52:33 +0330

    Custom Wrapper Functions

---
+++ Network Programming/intro/wrapfunctions.c
@@ -0,0 +1,98 @@
+#include	"unp.h"
+
+/*  Unix Functions Wrappers */
+
+/* include Colse */
+void
+Close(int fd)
+{
+	if (close(fd) == -1)
+		err_sys("close error");
+}
+/* end Colse */
+
+/* include Write */
+void
+Write(int fd, void *ptr, size_t nbytes)
+{
+	if (write(fd, ptr, nbytes) != nbytes)
+		err_sys("write error");
+}
+/* end Write */
+
+/* Socket Functions Wrappers */
+
+/* include Accept */
+int
+Accept(int fd, struct sockaddr *sa, socklen_t *salenptr)
+{
+	int		n;
+
+again:
+	if ( (n = accept(fd, sa, salenptr)) < 0) {
+#ifdef	EPROTO
+		if (errno == EPROTO || errno == ECONNABORTED)
+#else
+		if (errno == ECONNABORTED)
+#endif
+			goto again;
+		else
+			err_sys("accept error");
+	}
+	return(n);
+}
+/* end Accept */
+
+/* include Bind */
+void
+Bind(int fd, const struct sockaddr *sa, socklen_t salen)
+{
+	if (bind(fd, sa, salen) < 0)
+		err_sys("bind error");
+}
+/* end Bind */
+
+/* include Listen */
+void
+Listen(int fd, int backlog)
+{
+	char	*ptr;
+
+		/*4can override 2nd argument with environment variable */
+	if ( (ptr = getenv("LISTENQ")) != NULL)
+		backlog = atoi(ptr);
+
+	if (listen(fd, backlog) < 0)
+		err_sys("listen error");
+}
+/* end Listen */
+
+/* include Socket */
+int
+Socket(int family, int type, int protocol)
+{
+	int		n;
+
+	if ( (n = socket(family, type, protocol)) < 0)
+		err_sys("socket error");
+	return(n);
+}
+/* end Socket */
+
+/*  Library Functions Wrappers */
+
+/* include Inet_ntop */
+const char *
+Inet_ntop(int family, const void *addrptr, char *strptr, size_t len)
+{
+	const char	*ptr;
+
+	if (strptr == NULL)		/* check for old code */
+		err_quit("NULL 3rd argument to inet_ntop");
+	if ( (ptr = inet_ntop(family, addrptr, strptr, len)) == NULL)
+		err_sys("inet_ntop error");		/* sets errno */
+	return(ptr);
+}
+/* end Inet_ntop */
+
+
