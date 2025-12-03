cd75e25a653ef61b4ff2ad585f26f6e82257f9d1
farazattar <63309056+farazattar@users.noreply.github.com>
2025-12-03 09:26:26 +0330

    Version 3
    Can connect and also read values from the actual relay.

---
+++ IEC61850/iec61850_logger/v3/build/CMakeFiles/iec61850_logger.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/iec61850_logger.dir/main.c.obj"
+  "CMakeFiles/iec61850_logger.dir/main.c.obj.d"
+  "iec61850_logger.exe"
+  "iec61850_logger.exe.manifest"
+  "iec61850_logger.pdb"
+  "libiec61850_logger.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/iec61850_logger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
